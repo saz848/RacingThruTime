4aa3ec0e67b32f9f1db59879d544f1c53d12f2e6
Sarah Ahmad <sarahahmad2019@gmail.com>
2018-01-31 21:44:15 -0600

    you guys aren't gonna read this anyways

---
+++ Racing Thru Time.time/Racing Thru Time.time/Assets/Code/Waypoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+    public Waypoint north = null;
+    public Waypoint south = null;
+    public Waypoint east = null;
+    public Waypoint west = null; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
